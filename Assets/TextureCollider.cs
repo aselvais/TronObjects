de518db86dae2c5ce27816f603cdb5a3d2d836bb
Arnaud Selvais <aselvais@hostopia.com>
2017-07-05 13:10:45 -0400

    first commit

---
+++ Assets/TextureCollider.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * This attached to a game object will change the color of the object to the one colliding with it
+ */
+public class TextureCollider : MonoBehaviour {
+
+    /**
+     * Declare and initialize a new List of GameObjects called currentCollisions.
+     */
+    List<GameObject> currentCollisions = new List<GameObject>();
+
+    /**
+     * Container for the original color of the object
+     */
+    Color colorOriginal;
+
+    /**
+     * Container for all the Renderers composing this object
+     */
+    List<Renderer> thisObjects = new List<Renderer>();
+
+    /**
+     * 
+     */
+    private void Start()
+    {
+        var rdr = this.GetComponent<Renderer>();
+        if (rdr)
+        {
+            thisObjects.Add(rdr);
+            colorOriginal = rdr.material.color;
+        }
+        else
+        {
+            print("Getting the kids COLORS !!! ...");
+            int children = transform.childCount;
+            for (int i = 0; i < children; ++i)
+            {
+                thisObjects.Add(transform.GetChild(i).GetComponent<Renderer>());
+            }
+        }
+    }
+
+    /**
+     * When colliding with an object build the list of colliding objects and change the color or this one
+     */
+    void OnCollisionEnter(Collision col)
+    {
+
+        // Add the GameObject collided with to the list.
+        currentCollisions.Add(col.gameObject);
+
+        // Print the entire list to the console.
+        foreach (GameObject gObject in currentCollisions)
+        {
+            print("This " + name + " object touched the " + gObject.name + " object");
+            changeColor( gObject.GetComponent<Renderer>().material.color );
+        }
+    }
+
+    /**
+     * 
+     */
+    void OnCollisionExit(Collision col)
+    {
+        changeColor(colorOriginal);
+
+        // Remove the GameObject collided with from the list.
+        currentCollisions.Remove(col.gameObject);
+        // Print the entire list to the console.
+        foreach (GameObject gObject in currentCollisions)
+        {
+            print(gObject.name);
+        }
+    }
+
+    /**
+     * Change color of the current object (and all its kids)
+     */
+    void changeColor(Color newColor)
+    {
+        foreach (Renderer obj in thisObjects)
+        {
+            obj.material.color = newColor;
+        }
+    }
+}
---
+++ Assets/TextureMapper.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/**
+ * Class to attach to a texture enabeling a color change dynamically
+ * 
+ * @author Arnaud Selvais for Hostopia
+ */
+public class TextureMapper : MonoBehaviour {
+
+    /**
+     * Target color
+     */
+    Color newColor;
+    /**
+     * RGB values
+     */
+    float color_r = 0F;
+    float color_g = 1F;
+    float color_b = 0F;
+    /**
+     * Amount of time it takes to fade between colors
+     */
+    float transitionTime = 1f;
+    /**
+     * 
+     */
+ //   bool activate = true;
+
+    /**
+     * Renderer of our object
+     */
+    Renderer thisRend;
+
+    /**
+     * 
+     */
+    void Start()
+    {
+        // grab the renderer component on our cube
+        thisRend = GetComponent<Renderer>();
+        newColor = new Color(color_r, color_g, color_b, 1F);
+        // start our coroutine when the game starts
+        StartCoroutine(ColorChange());
+    }
+
+    /**
+     * 
+     */
+    void Update()
+    {
+    }
+
+    /**
+     * 
+     */
+    IEnumerator ColorChange()
+    {
+        // Infinite loop will ensure our coroutine runs all game
+        while (true)
+        {
+            // Create and set transitionRate to 0. This is necessary for our next while loop to function
+            float transitionRate = 0;
+            /**
+             * 1 is the highest value that the Color.Lerp function uses for
+             * transitioning between two colors. This while loop will execute
+             * until transitionRate is incremented to 1 or higher
+             */
+            while (transitionRate < 1)
+            {
+                // this next line is how we change our material color property. We Lerp between the current color and newColor
+                thisRend.material.SetColor("_Color", 
+                    Color.Lerp(
+                        thisRend.material.color, 
+                        newColor, 
+                        Time.deltaTime * transitionRate
+                    )
+                );
+                // Increment transitionRate over the length of transitionTime
+                transitionRate += Time.deltaTime / transitionTime;
+                // wait for a frame then loop again
+                yield return null;
+            }
+            // wait for a frame then loop again
+            yield return null; 
+        }
+    }
+}
